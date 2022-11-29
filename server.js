7699db115db4b13a3de8d35648d709a2c597f9f9
OhBeomho <tyler0322@naver.com>
2022-11-29 17:53:23 +0900

    public directory

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("view engine", "ejs")
 app.set("views", "src")
 app.engine("html", require("ejs").renderFile)
 
-app.use(express.static("src"))
+app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(
